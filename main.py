f7e20488921830000273f759d0290f06223407fe
steves17 <steves17@mcmaster.ca>
2024-12-19 23:32:26 +0800

    more csv_actions

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ import os
 from .helpers.config import config_actions
 from .helpers.csv import csv_actions
 config_actions.setup_cfg()
-csv_actions.generate_empty_availability_data(start_from_scratch=True)
+data = csv_actions.generate_empty_availability_data(start_from_scratch=True)
+csv_actions.write_availability_data(data, 'new_sample.csv')
